5454640d3034ca6e3b59196a4b6561a602e1cb1b
Sandra Walsh <sandra.walsh@appliedsystems.com>
2021-09-09 09:53:45 +0100

    Search bar wip

--- src/Components/PokemonList.js
+++ src/Components/PokemonList.js
@@ -2,6 +2,7 @@ import './PokemonList.css';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import Loading from './Loading';
+import SearchBar from './SearchBar';
 import { FormatPokemonName } from '../Utils/Utils';
 
 export default function PokemonList(props) {
@@ -10,8 +11,10 @@ export default function PokemonList(props) {
     };
 
     const [allPokemon, setAllPokemon] = useState([]);
+    const [pokemonList, setPokemonList] = useState([]);
     const [currentUrl, setCurrentUrl] = useState('https://pokeapi.co/api/v2/pokemon/');
     const [loading, setLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState(null);
     
     // Every time currentUrl changes
     // Re-run code inside useEffect to get next page of Pokemon
@@ -33,11 +36,24 @@ export default function PokemonList(props) {
           setCurrentUrl(res.data.next);
         } else {
           setLoading(false);
+          setPokemonList(allPokemon);
         }
       });
   
       return () => cancel();
     }, [currentUrl]);
+
+    useEffect(() => {
+        if (searchTerm) {
+            const filteredList = allPokemon.filter(pokemon => {
+                return pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+            });
+
+            setPokemonList(filteredList);
+        } else {
+            setPokemonList(allPokemon);
+        }
+    }, [searchTerm]);
 
     if (loading) return <div className="PokemonList">
       <Loading />
@@ -45,8 +61,10 @@ export default function PokemonList(props) {
 
     return (
         <div className="PokemonList">
+          <SearchBar setSearchTerm={setSearchTerm} />
+          <p className="row-item" hidden={pokemonList.length > 0}>No Pokemon found</p>
           <ol>
-            {allPokemon.map((pokemon) => 
+            {pokemonList.map((pokemon) => 
                 <li key={pokemon.name} className="Pokemon" onClick={function(e) {
                     selectPokemon(pokemon.url);
                   }}>
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './SearchBar.css';
 
-export default function SearchBar() {
+export default function SearchBar(props) {
+    function updateSearchTerm(searchTerm) {
+        props.setSearchTerm(searchTerm);
+    };
+
     return (
-        <div>
-            
+        <div className="SearchBar">
+            <input className="input"
+                key="SearchBar"
+                placeholder={"Search"}
+                onChange={(e) => updateSearchTerm(e.target.value)}
+            />
         </div>
     )
 }
